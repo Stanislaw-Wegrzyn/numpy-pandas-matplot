f0a4f03cf019501cd2c9aebdbec00ce98ef8a4ad
Stanisław Węgrzyn <128520818+Stanislaw-Wegrzyn@users.noreply.github.com>
2023-03-22 18:59:39 +0000

    Delete App directory

--- App/items/py/screen_vs_run.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import time
-
-subprocess.run(['pkill', 'screen', '-9'])
-subprocess.run(['screen', '-wipe'])
-subprocess.run(['screen', '-S', 'vs', '-dm', 'bash', '-c', './velocitysniper'])
-time.sleep(1.1)
-subprocess.run(['screen', '-ls'])
-print('Done.')
